ef3b8fbf7cb78cf8194b35c67d775ec44274e2b1
kimprojects <kimpita9@gmail.com>
2018-01-26 17:50:28 +0900

    calculator done

--- js/calc.js
+++ js/calc.js
@@ -1,13 +1,13 @@
-function calculate(event)
+function calculate(event, unhide=false)
 {
   event.preventDefault();
   if ($('#buffet').prop('checked')) {var buffet=$('#buffet').val();}else{var buffet=0;}
   if ($('#traditional').prop('checked')) {var traditional=$('#traditional').val();}else{var traditional=0;}
   if ($('#beverages').prop('checked')) {var beverages=$('#beverages').val();}else{var beverages=0;}
   if ($('#snacks').prop('checked')) {var snacks=$('#snacks').val();}else{var snacks=0;}
-  if($('#start_date').val()=='' || $('#end_date').val()==''){alert("Select dates!");}else{
   var guests=$('#guests').val();
   var transport=$('#location').val();
+  var start_time=$('#start_time').val();
   var service=5000;
   var cost_per_plate=parseFloat(buffet) + parseFloat(traditional);
   var cost_per_beverage=parseFloat(beverages) + parseFloat(snacks);
@@ -15,10 +15,20 @@ function calculate(event)
   var drinks=(cost_per_beverage * guests);
   var food_drinks=parseFloat(food)+parseFloat(drinks);
   var total_cost=parseFloat(food_drinks)+parseFloat(transport)+parseFloat(service);
-  $('#transport_cost').html('Ksh. '+transport);
-  $('#food_drink_cost').html('Ksh. '+food_drinks);
-  $('#service_charge').html('Ksh. '+service);
-  $('#total_cost').html('Ksh. '+total_cost);
-  $('.calc-fonts li').removeClass('hidden');
- }
+  $('#transport_cost').html('Ksh. '+$.number( transport, 2 ));
+  $('.location').val(transport);
+  $('#food_drink_cost').html('Ksh. '+$.number( food_drinks, 2 ));
+  $('#service_charge').html('Ksh. '+$.number( service, 2 ));
+  $('#total_cost').html('Ksh. '+$.number( total_cost, 2 ));
+  $('.total_cost').val('Total Cost: '+$.number( total_cost, 2 )+'/=');
+  $('.guests').val('Max Guests: '+guests);
+  $('.start_time').val(start_time);
+  $('.start_date').val('Starts on: '+$('#start_date').val());
+  if(unhide){
+    if($('#start_date').val()==''){
+      alert("Select date!");
+    }else {
+      $('.calc-fonts li').removeClass('hidden');
+    }
+  }
 }
